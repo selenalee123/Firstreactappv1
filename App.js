8ffc2fec1f7fa159aab35efd4d14d56046993a01
Kim Kim <52686208+selenalee123@users.noreply.github.com>
2020-11-01 07:50:27 -0500

    Main

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text} from 'react-native';
+//import Pro from "./src/screens/Pro";
+import Test from "./src/screens/Test";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import Profilescreen from "./src/screens/Profilescreen";
+//import Onboarding from "./src/screen/Onboarding"
+
+
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <View style={styles.container}>
+          <Test/>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View,Image,Button,TouchableOpacity} from "react-native";
+
+//const getPro = require("./assets/imgs/bg.png");
+//const Invision = require("./assets/imgs/invision-white-slim.png");
+//const NowLogo = require("./assets/imgs/now-logo.png");
+//const CreativeTim = require("./assets/imgs/creative-tim-white-slim2.png");
+//const ios = require("./assets/imgs/ios.png");
+//const android = require("./assets/imgs/android.png");
+
+
+
+
+export default class Test extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+    render() {
+  return (
+      <View>
+          <Text>test</Text>
+      </View>
+  )
+    }
+}
